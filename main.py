7e689573f59364742ee19dd111a5a509cc9b943b
ian <leonpowell2013@gmail.com>
2024-06-13 08:47:48 +0100

    4th commit - added basic movement to the snail image

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ FPS = 60
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface =  test_font.render('My game', False, 'Purple')
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
+
 while True:
 
     for event in pygame.event.get():
@@ -25,5 +30,11 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos,270))
+
     pygame.display.update()
     clock.tick(FPS)
