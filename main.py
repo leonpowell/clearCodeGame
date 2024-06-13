660b867b7633e530ecb120238e642c14cd43c726
ian <leonpowell2013@gmail.com>
2024-06-13 09:02:41 +0100

    5th commit - added player image and used get_rect() to position on the surface

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface =  test_font.render('My game', False, 'Purple')
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
 
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(topleft = (80,215))
 while True:
 
     for event in pygame.event.get():
@@ -35,6 +38,6 @@ while True:
     if snail_x_pos < -100:
         snail_x_pos = 800
     screen.blit(snail_surface, (snail_x_pos,270))
-
+    screen.blit(player_surf, player_rect)
     pygame.display.update()
     clock.tick(FPS)
