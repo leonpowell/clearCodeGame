7f073d032f831f611be5799b3e72d071c0a72daf
ian <leonpowell2013@gmail.com>
2024-06-15 09:47:10 +0100

    8th commit - added code for floor

--- main.py
+++ main.py
@@ -60,6 +60,8 @@ while True:
     # player
     player_gravity += 1
     player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
 
     pygame.display.update()
