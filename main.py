301326849c1b2bf339ab8ca08e95bbc1be1798e0
ian <leonpowell2013@gmail.com>
2024-06-15 09:41:25 +0100

    7th commit - added gravity to player character and jump mechanics with space bar and mouse button down method

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, 'Purple')
+score_surf = test_font.render('My game', False, (64,64,64))
 score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_x_pos = 600
@@ -27,26 +27,40 @@ snail_rect = snail_surf.get_rect(topleft = (snail_x_pos,270))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(topleft = (80,230))
+
+player_gravity = 0
+
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all out elements and update everything
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+
+    # draw all out elements and update everything
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, 'Yellow', score_rect)
-    pygame.draw.rect(screen, 'Yellow', score_rect, 10)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
     screen.blit(score_surf, score_rect)
-
-
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
+
+    # player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf, player_rect)
+
     pygame.display.update()
     clock.tick(FPS)
