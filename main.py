58e2d24e22991e6e9e30ef4e82110405946b9b35
ian <leonpowell2013@gmail.com>
2024-06-16 07:19:48 +0100

    10th commit - added game over state and basic screen which will become menu screen

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
+
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
@@ -45,24 +47,31 @@ while True:
             if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
                 player_gravity = -20
 
-    # draw all out elements and update everything
-    screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-    screen.blit(score_surf, score_rect)
+    if game_active:
+        # draw all out elements and update everything
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface, (0, 300))
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        screen.blit(score_surf, score_rect)
 
-    snail_rect.x -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800
-    screen.blit(snail_surf, snail_rect)
+        snail_rect.x -= 4
+        if snail_rect.right <= 0:
+            snail_rect.left = 800
+        screen.blit(snail_surf, snail_rect)
 
-    # player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
-    screen.blit(player_surf, player_rect)
+        # player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Blue')
+
 
     pygame.display.update()
     clock.tick(FPS)
