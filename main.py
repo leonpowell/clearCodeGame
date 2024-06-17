72dab491c145c98a8b352c99547b2654f3e51886
ian <leonpowell2013@gmail.com>
2024-06-17 08:43:20 +0100

    12th commit - added timer to game to act as a score

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
 from sys import exit
-pygame.init()
 
+
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+
+
+
+
+pygame.init()
 
 # settings
 width = 800
 height = 400
 FPS = 60
-
+start_time = 0
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Runner")
@@ -19,16 +29,14 @@ game_active = True
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, (64,64,64))
-score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_x_pos = 600
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(topleft = (snail_x_pos,270))
+snail_rect = snail_surf.get_rect(topleft=(snail_x_pos, 270))
 
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(topleft = (80,230))
+player_rect = player_surf.get_rect(topleft=(80, 230))
 
 player_gravity = 0
 
@@ -52,14 +60,16 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         # draw all out elements and update everything
-        screen.blit(sky_surface,(0,0))
+        screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        screen.blit(score_surf, score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0:
@@ -77,7 +87,6 @@ while True:
             game_active = False
     else:
         screen.fill('Blue')
-
 
     pygame.display.update()
     clock.tick(FPS)
